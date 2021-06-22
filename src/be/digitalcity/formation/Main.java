30b23f85eb19681b46871f403c88022498409dad
local_user <navez.martin@gmail.com>
2021-06-22 11:34:48 +0200

    Start

---
+++ src/be/digitalcity/formation/Main.java
@@ -0,0 +1,17 @@
+package be.digitalcity.formation;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Opérateur ternaire
+
+        int a = 0;
+
+        if (a < 10){
+            a = 20;
+        } else {
+            a = 30;
+        }
+    }
+}
