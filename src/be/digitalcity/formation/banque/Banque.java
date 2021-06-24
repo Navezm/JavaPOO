ba98126a569b4724f424e1df0f5ed72a073f16c0
Navezm <navez.martin@gmail.com>
2021-06-24 13:08:19 +0200

    Banque | Corrections

--- src/be/digitalcity/formation/banque/Banque.java
+++ src/be/digitalcity/formation/banque/Banque.java
@@ -1,38 +1,94 @@
 package be.digitalcity.formation.banque;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
 
 public class Banque {
-    private String nom;
-    private HashMap<String, Courant> allComptes = new HashMap<>();
+    private final String NOM;
+    private final HashMap<String, Courant> allComptes = new HashMap<>();
+
+    public Banque(String nom) {
+        this.NOM = nom;
+    }
 
     public String getNom() {
-        return nom;
+        return NOM;
     }
 
-    public void search(String numeroCompte){
-        if (allComptes.containsKey(numeroCompte)) {
-            System.out.printf("Voici les informations du compte %10s\n" +
-                    "Nom du titulaire : " +allComptes.get(numeroCompte).getTitulaire().getNom()+" "+allComptes.get(numeroCompte).getTitulaire().getPrenom()+"\n"+
-                    "Solde : "+allComptes.get(numeroCompte).getSolde()+"\n"+
-                    "Ligne de crédit : "+allComptes.get(numeroCompte).getLigneDeCredit()+"\n",numeroCompte);
+    public HashMap<String, Courant> getAllComptes() {
+        return allComptes;
+    }
+
+    public Courant search(String numeroCompte) {
+        if (numeroCompte != null) {
+            if (this.allComptes.containsKey(numeroCompte)) {
+                System.out.printf("Voici les informations du compte %10s\n" +
+                        "Nom du titulaire : " + this.allComptes.get(numeroCompte).getTitulaire().getNom() + " " + this.allComptes.get(numeroCompte).getTitulaire().getPrenom() + "\n" +
+                        "Solde : " + this.allComptes.get(numeroCompte).getSolde() + "\n" +
+                        "Ligne de crédit : " + this.allComptes.get(numeroCompte).getLigneDeCredit() + "\n", numeroCompte);
+                return this.allComptes.get(numeroCompte);
+            } else {
+                System.out.printf("Le compte %10s n'a pas été trouvé\n", numeroCompte);
+            }
         } else {
-            System.out.println("Le compte n'existe pas !");
+            System.err.println("Le numéro de compte n'est pas valide");
+        }
+        return null;
+    }
+
+    public void ajouter(Courant compte) {
+        if (compte != null) {
+            this.allComptes.put(compte.getNumero(), compte);
+            System.out.printf("Le compte n°%1s du tutilaire %1s a correctement été ajouté à ma banque\n", compte.getNumero(), compte.getTitulaire().getNom());
+        } else {
+            System.err.println("Le compte est vide, il n'est pas possible de l'ajouter");
         }
     }
 
-    public void ajouter(Courant compte){
-        allComptes.put(compte.getNumero(), compte);
-        System.out.printf("Le compte n°%10s du tutilaire %10s a correctement été ajouté à ma banque\n",compte.getNumero(),compte.getTitulaire().getNom());
+    public void supprimer(String numeroCompte) {
+        if (this.allComptes.remove(numeroCompte) != null) {
+            System.out.printf("Le compte N°%1s a bien été supprimé\n", numeroCompte);
+        } else {
+            System.err.printf("Le compte %1s n'existe pas ! Suppression impossible\n", numeroCompte);
+        }
     }
 
-    public void supprimer(String numeroCompte){
-        if (allComptes.containsKey(numeroCompte)) {
-            allComptes.remove(numeroCompte);
-            System.out.printf("Le compte N°%10s a bien été supprimé de la DB\n",numeroCompte);
-        } else {
-            System.out.println("Le compte n'existe pas ! Suppresion impossible");
+    public double totalAvoir(Personne titulaire) {
+        if (titulaire != null) {
+            /*
+            // Map des données de type Entry<V,V> dans la variable entry (après les ":" on rentre ce qu'on veut mapper)
+            for (Map.Entry<String, Courant> entry :
+                    allComptes.entrySet()) {
+                if (entry.getValue().getSolde() > 0) {
+                    total += entry.getValue().getSolde();
+                }
+            }
+            System.out.println("Le total des comptes de "+ titulaire.getNom() + " " + titulaire.getPrenom() + " est de : " + total);
+             */
+
+            double total = 0;
+            int compteur = 0;
+            for (Courant compte : this.allComptes.values()) {
+                if (compte.getTitulaire().equals(titulaire)) {
+                    if (compte.getSolde() > 0) {
+                        total += compte.getSolde();
+                        compteur ++;
+                    }
+                }
+            }
+            if (compteur == 0) {
+                System.out.printf("Le titulaire %25s ne possède pas de compte\n",
+                        titulaire.getPrenom()+" "+titulaire.getNom());
+            } else {
+                System.out.printf("Total des avoirs de %25s : %.2fEUR\n",
+                        titulaire.getPrenom()+" "+titulaire.getNom(),
+                        total);
+            }
+            return total;
         }
+        System.err.println("Problème de données non valide");
+        return 0;
     }
 
 }
